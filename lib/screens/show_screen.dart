1e5c6711b9a7c4edf7d3fc0565cd46cc85a8d3b9
Petar Jovanovic <jovanovic.petar499@gmail.com>
2022-03-17 20:44:37 +0100

    Fixed a typo

--- lib/screens/show_screen.dart
+++ lib/screens/show_screen.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:tv_shows/classes/show.dart';
-import 'package:tv_shows/gen/fonts.gen.dart';
 import 'package:tv_shows/widgets/hidden_shows_widget.dart';
+import 'package:tv_shows/widgets/show_screen_top_widget.dart';
 import 'package:tv_shows/widgets/shows_list_widget.dart';
 
 class ShowScreen extends StatefulWidget {
@@ -13,8 +13,6 @@ class ShowScreen extends StatefulWidget {
 }
 
 class _ShowScreenState extends State<ShowScreen> {
-  bool _isHidden = false;
-
   @override
   Widget build(BuildContext context) {
     return MultiProvider(
@@ -32,35 +30,10 @@ class _ShowScreenState extends State<ShowScreen> {
             return Column(
               children: [
                 const SizedBox(height: 44),
-                Padding(
-                  padding: const EdgeInsets.symmetric(vertical: 16.0, horizontal: 24),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Text(
-                        'Shows',
-                        style: Theme.of(context).textTheme.headline1,
-                      ),
-                      OutlinedButton(
-                        onPressed: () {
-                          setState(() => _isHidden = !_isHidden);
-                        },
-                        child: Text(
-                          _isHidden ? 'Show' : 'Hide',
-                          style: const TextStyle(fontFamily: FontFamily.roboto),
-                        ),
-                        style: ButtonStyle(
-                          overlayColor: MaterialStateProperty.all(Theme.of(context).primaryColor),
-                          padding: MaterialStateProperty.all(const EdgeInsets.symmetric(horizontal: 24, vertical: 12)),
-                          shape: MaterialStateProperty.all(
-                              RoundedRectangleBorder(borderRadius: BorderRadius.circular(24))),
-                          side: MaterialStateProperty.all(const BorderSide(color: Colors.black38)),
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-                _isHidden ? HiddenShowsWidget(constraints) : ShowsListWidget(constraints),
+                const ShowScreenTopWidget(),
+                Provider.of<ShowsScreenContentProvider>(context).isHidden
+                    ? HiddenShowsWidget(constraints)
+                    : ShowsListWidget(constraints),
               ],
             );
           },
